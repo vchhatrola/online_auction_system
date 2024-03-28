58d93d25c1b0013fc3d818ec33f0006d2bc5b127
vchhatrola <161204350+vchhatrola@users.noreply.github.com>
2024-03-28 21:30:52 +0530

    login changes and bootstap added

--- auctionvidhi/client/src/Components/Signup.js
+++ auctionvidhi/client/src/Components/Signup.js
@@ -1,170 +1,108 @@
-/*import React ,{useState} from 'react'
-import '../App.css'
-import Axios from 'axios'
-import {Link, useNavigate} from 'react-router-dom'
+import React from 'react';
+import Axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form"
 
 const Signup = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate()
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        Axios.post('http://localhost:3000/auth/signup', {  //never change port number
-            username,
-            email,
-            password,
-        }).then(response =>{
-            if(response.data.status){
-           // console.log(response)
-           navigate('/login')
-            } 
-
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data, "data");
+        Axios.post("http://localhost:3000/auth/signup", data).then(response => {
+            if (response.data.status) {
+                navigate('/login')
+            }
         }).catch(err => {
             console.log(err)
-          })
+        })
+
+        // const handleChange = (e) => {
+        //     // Allow only numeric characters
+        //     const input = e.target.value.replace(/\D/g, '');
+        //     setAdharCardNumber(input);
+        // };
+        // const handleChange1 = (e) => {
+        //     // Allow only numeric characters
+        //     const input = e.target.value.replace(/\D/g, '');
+        //     setPhoneNumber(input);
+        // };
     }
 
     return (
-        <div className='sign-up-container'>
-            <form className='sign-up-form' onSubmit={handleSubmit}>
-                <h2>Sign Up</h2>
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder='Username'
-                    onChange={(e) => setUsername(e.target.value)} />
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <h2 className="text-center mb-4">Sign Up</h2>
+                                <div className="form-group">
+                                    <label htmlFor="username">Username</label>
+                                    <input type="text" className="form-control" id="username" placeholder="Username"
+                                        {...register("username", { required: true })} />
+                                    {errors.username && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email address</label>
+                                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"
+                                        {...register("email", { required: true })} />
+                                    {errors.email && <span className="text-danger">This field is required</span>}
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password1">Password</label>
+                                    <input type="password" className="form-control" id="password1" placeholder="Password"
+                                        {...register("password", { required: true })} />
+                                    {errors.password && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="firstName">First Name</label>
+                                    <input type="text" className="form-control" id="firstName" placeholder="First Name"
+                                        {...register("firstName", { required: true })} />
+                                    {errors.firstName && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="lastName">Last Name</label>
+                                    <input type="text" className="form-control" id="lastName" placeholder="Last Name"
+                                        {...register("lastName", { required: true })} />
+                                    {errors.lastName && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="phoneNumber">Phone Number</label>
+                                    <input type="text" className="form-control" id="phoneNumber" placeholder="Phone Number" maxLength="10" 
+                                        {...register("phoneNumber", { required: true })} />
+                                    {errors.phoneNumber && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="address">Address</label>
+                                    <textarea class="form-control" id="address" rows="3"  {...register("address", { required: true })}></textarea>
+                                    {errors.address && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="registrationDate">Registration Date</label>
+                                    <input type="date" className="form-control" id="registrationDate" placeholder="Registration Date"
+                                        {...register("registrationDate", { required: true })} />
+                                    {errors.registrationDate && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="adharCardNumber">Adhar Card Number</label>
+                                    <input type="text" className="form-control" id="adharCardNumber" placeholder="Adhar Card Number" maxLength="12"
+                                        {...register("adharCardNumber", { required: true })} />
+                                    {errors.adharCardNumber && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" {...register("termCondition", { required: true })} />
+                                    <label className="form-check-label" htmlFor="exampleCheck1" > I agree <Link to="/termsandconditions">terms and conditions</Link></label>
+                                    {errors.termCondition && <span className="text-danger">This field is required</span>}
+                                </div>
+                                <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                                <p className="mt-2">Have an account? <Link to="/login">Login</Link></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                <label htmlFor="email">Email</label>
-                <input type="email" autoComplete='off' placeholder='Email'
-                    onChange={(e) => setEmail(e.target.value)} />
-
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder='******'
-                    onChange={(e) => setPassword(e.target.value)} />
-
-                <button type='submit'>Sign Up</button>
-                <p>Have an account? <Link to="/login">Login</Link></p>
-            </form>
-        </div>
-    )
-}
-
-export default Signup */
-
-//blackbox code
-
-import React, { useState } from 'react';
-import '../App.css';
-import Axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-
-
-const Signup = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [address, setAddress] = useState('');
-    const [registrationDate, setRegistrationDate] = useState('');
-    const [adharCardNumber, setAdharCardNumber] = useState('');
-    const [checked, setChecked] = useState(false);
-   
-
-
-    const navigate = useNavigate();
-
-   const handleSubmit = (e) => {
-        e.preventDefault();
-        Axios.post('http://localhost:3000/auth/signup', {
-            username,
-            email,
-            password,
-            firstName,
-            lastName,
-            phoneNumber,
-            address,
-            registrationDate,
-            adharCardNumber,
-
-        }).then((response) => {
-            if (response.data.status) {
-                navigate('/login');
-            }
-        }).catch((err) => {
-            console.log(err);
-        });
-    };
-  
-    const handleChange = (e) => {
-        // Allow only numeric characters
-        const input = e.target.value.replace(/\D/g, '');
-        setAdharCardNumber(input);
-    };
-    const handleChange1 = (e) => {
-        // Allow only numeric characters
-        const input = e.target.value.replace(/\D/g, '');
-        setPhoneNumber(input);
-    };
-    
-
-    return (
-        <div className='sign-up-container1'>
-            <form className='sign-up-form1' onSubmit={handleSubmit} >
-                <h2>Sign Up</h2>
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder='Username' required
-                    onChange={(e) => setUsername(e.target.value)} />
-
-                <label htmlFor="email">Email</label>
-                <input type="email" autoComplete='off' placeholder='Email' required
-                    onChange={(e) => setEmail(e.target.value)} />
-
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder='******' required
-                    onChange={(e) => setPassword(e.target.value)} />
-
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" placeholder='First Name' required
-                    onChange={(e) => setFirstName(e.target.value)} />
-
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" placeholder='Last Name' required
-                    onChange={(e) => setLastName(e.target.value)} />
-
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input type="text" placeholder='Phone Number' required
-                    maxLength="10"
-                    value={phoneNumber}
-                    onChange={handleChange1}
-                
-                />
-
-                
-                <label htmlFor="address">Address<textarea type="text" cols="30" rows="6" placeholder='Address'
-                    onChange={(e) => setAddress(e.target.value)} required></textarea></label>
-
-                <label htmlFor="registrationDate">Registration Date</label>
-                <input type="date" placeholder='Registration Date' required
-                    onChange={(e) => setRegistrationDate(e.target.value)} />
-
-                <label htmlFor="adharCardNumber">Adhar Card Number</label>
-                <input type="text" placeholder='Adhar Card Number' required
-                    maxLength="12"
-                    value={adharCardNumber}
-                    onChange={handleChange}
-                />
-
-                <label >
-                    <input type="checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)} />
-                    I agree <Link to="/termsandconditions">terms and conditions</Link>
-                </label>
-
-                <button type='submit' disabled={!checked}>Sign Up</button>
-                <p>Have an account? <Link to="/login">Login</Link></p>
-            </form>
-           
         </div>
     );
 };
